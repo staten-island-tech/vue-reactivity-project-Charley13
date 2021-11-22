dc0ea07f106e6aaacb83c7d062c7c5712d448fa9
Charley13 <charley.baluja22@sitechhs.com>
2021-11-21 22:08:15 -0500

    part one

---
+++ mushroom-list/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+ <div class="app-overall">
+<!-- Picture link: https://www.wallpaperflare.com/white-clouds-and-green-grass-field-wallpaper-pixel-art-pixels-digital-art-horizon-wallpaper-243952-->
+   <div class="heading-box">
+   <h1 class="heading app-title">Mushroom List</h1>
+   <h2 class="heading app-subtitle">Complete tasks to add mushrooms + create a beautiful garden!</h2>
+   </div>
+   <ToDo />
+<iframe class="video" width="150" height="100" src="https://www.youtube.com/embed/tBDVLey1DIE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe>
+ </div>
+</template>
+
+<script>
+import ToDo from './components/ToDo.vue'
+
+export default {
+  name: 'App',
+  components: {
+     ToDo
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&family=Indie+Flower&display=swap');
+
+html,body{
+  margin: 0;
+}
+.app-overall{
+  width: 100vw;
+   height: 100vh;  
+  background-image: url(./assets/background.jpg);
+  background-size: contain;
+}
+.heading-box{
+  width: 100%;
+  display: flex;
+flex-direction: column;
+ justify-content: center;
+ align-items: center;
+ padding-top: 2.3rem;
+}
+.heading{
+  margin: 0;
+  color: white;
+}
+.app-title{
+  font-family: 'Amatic SC', cursive;
+  font-size: 4.5rem;
+}
+.app-subtitle{
+  font-family: 'Indie Flower', cursive;
+  font-size: 1.5rem;
+}
+.video{
+ position: absolute;
+ right: 1rem;
+ top: 1rem;
+}
+</style>
---
+++ mushroom-list/src/components/ToDo.vue
@@ -0,0 +1,91 @@
+<template>
+ <div class="to-do-overall">
+   <div class="box to-do-box">
+     <h3 class="box-title">To-Do Box:</h3>
+     <ul class="list">
+       <li> 
+         <input class="text-input"/>
+       <input @click="finished" class="checkbox" type="checkbox">
+       </li>
+        <li> 
+         <input class="text-input"/>
+      <input @click="finished" class="checkbox" type="checkbox">
+       </li> 
+        <li> 
+         <input class="text-input"/>
+       <input @click="finished" class="checkbox" type="checkbox">
+       </li>
+        <li> 
+         <input class="text-input"/>
+       <input @click="finished" class="checkbox" type="checkbox">
+       </li>
+        <li> 
+         <input class="text-input"/>
+      <input @click="finished" class="checkbox" type="checkbox">
+       </li>
+     </ul>
+   </div>
+   <div class=" box finished-box">
+     <h3 class="box-title">Finished Box:</h3>
+   </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDo', 
+  methods: {
+    finished(){
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+.to-do-overall{
+  width: 100%;
+  display: flex;
+}
+.box{
+  width: 20rem;
+  height: 13rem;
+  border: white solid 0.1rem;
+  border-radius: .3rem;
+  margin-top: 3rem;
+  background-color: #ffe3c6;
+}
+.to-do-box{
+  margin-left: 4rem;
+}
+.finished-box{
+  position: absolute;
+  right: 4rem;
+}
+.box-title{
+  margin-top: 0.5rem;
+  margin-left: 0.5rem;
+  margin-bottom: 0.2rem;
+  text-decoration: underline;
+  font-family: 'Indie Flower', cursive;
+}
+.list{
+    margin: 0;
+  padding: 0;
+  margin-left: 2rem;
+}
+.text-input{
+  width: 80%;
+  height: 10%;
+  border: none;
+  outline: none;
+  border-bottom: solid 0.1rem black;
+  margin-bottom: 0.5rem;
+    background-color: #ffe3c6;
+}
+.checkbox{
+  margin-left: 1rem;
+  width: 1rem;
+  height: 1rem;
+}
+</style>
